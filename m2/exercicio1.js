5ccc7ab58bc3468d2a8842b541b667e0a2250ea7
anabsena7-cpu <ana.b.sena7@aluno.senai.br>
2025-09-15 15:43:30 -0300

    Update exercicio1.js

--- m2/exercicio1.js
+++ m2/exercicio1.js
@@ -1,18 +1,8 @@
+let nome = "Ana";
+let idade = 17;
+let cidade = "Sorocaba"
 
-let minhaString = "Olá, mundo!";
-let meuNumero = 30;
-let meuBooleano = true;
+console.log(nome);
+console.log(idade);
+console.log(cidade);
 
-console.log (`Valor: ${minhaStrings}, Tipo: ${typeof minhaString}`);
-console.log (`Valor: ${meuNumero}, Tipo: ${typeof meuNumero}`);
-console.log (`Valor: ${meuBooleano}, Tipo: ${typeof meuBooleano}`);
-
-let numeroParaString = 123;
-let stringConvertida = String(numeroParaString);
-
-console.log(`Resultado da conversão: ${stringConvertida}, Tipo ${typeof stringConvertida}`);
-
-let stringNumerica = "465";
-let numeroConvertido = Number(stringNumerica);
-
-console.log(`Resultado da conversão: ${numeroConvertido}, Tipo: ${numeroConvertido}`);
