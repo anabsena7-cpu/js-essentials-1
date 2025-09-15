c07928cc32c175544c327e1533ea33227d84a46e
anabsena7-cpu <ana.b.sena7@aluno.senai.br>
2025-09-15 15:50:50 -0300

    Update exercicio8 .js

--- m2/exercicio8 .js
+++ m2/exercicio8 .js
@@ -1,8 +1,15 @@
-console.log(`0: ${Boolean(0)}`);
-console.log(`1: ${Boolean(1)}`);
-console.log(`"": ${Boolean("")}`);
-console.log(`"Hello": ${Boolean("Hello")}`);
-console.log(`null: ${Boolean(null)}`);
-console.log(`undefined: ${Boolean(undefined)}`);
-console.log(`{}: ${Boolean({})}`);
-console.log(`[]: ${Boolean([])}`);
+if (true) {
+  var x = 10;
+  let y = 20;
+  const z = 30;
+
+  console.log("Dentro do bloco:");
+  console.log("x =", x); // 10
+  console.log("y =", y); // 20
+  console.log("z =", z); // 30
+}
+
+console.log("Fora do bloco:");
+console.log("x =", x); // 10 — `var` ignora o escopo de bloco
+console.log("y =", y); // Erro: y is not defined, pq está dentro do bloco
+console.log("z =", z); //Erro: z is not defined, pq está dentro do bloco
