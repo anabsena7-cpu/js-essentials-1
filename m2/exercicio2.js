694ecd263010be9c6586e9e9c9b4d1b55cd6d707
anabsena7-cpu <ana.b.sena7@aluno.senai.br>
2025-09-15 15:43:48 -0300

    Update exercicio2.js

--- m2/exercicio2.js
+++ m2/exercicio2.js
@@ -1,15 +1,10 @@
-let valorNulo = null;
-let valorIndefinido = undefined;
-
-console.log(`Tipo de null: ${typeof valorNulo}`);
-console.log(`Tipo de undefined: ${typeof valorIndefinido}`);
-
-let meuObjeto = {};
-let meuArray = [];
-
-console.log(`Tipo de um objeto: ${typeof meuObjeto}`);
-console.log(`Tipo de um array: ${typeof meuArray}`);
-
-let meuBigInt = 50098576479208;
-
-console.log(`Tipo de um BigInt: ${typeof meuBigInt}`);
+//var 2nome; inválido (não pode começar com 2)
+var nome2;
+//var _idade; válido
+var _idade;
+//var cidade-natal; inválido (por conta do -)
+var cidade_natal;
+//var $preco; válido
+var $preco;
+//var let; inválido, let é um nome q tem uma função ja no js, entao nao pode ser usado como variavel
+var var_let;
