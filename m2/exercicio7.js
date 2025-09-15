81f6efe848217754dcf589e0fb471ddeada94bd2
anabsena7-cpu <ana.b.sena7@aluno.senai.br>
2025-09-15 15:50:32 -0300

    Update exercicio7.js

--- m2/exercicio7.js
+++ m2/exercicio7.js
@@ -1,12 +1,6 @@
-console.log(`"123": ${Number("123")}`);
-console.log(`"123.45": ${Number("123.45")}`);
-console.log(`"": ${Number("")}`);
-console.log(`"abc": ${Number("abc")}`);
-console.log(`"0": ${Number("0")}`);
+const pi = 3.14;
+console.log(pi);
 
-let strInt = "123.45";
-let strFloat = "123.45";
-
-console.log(`parseInt("${strInt}"): ${parseInt(strInt)}`);
-console.log(`parseFloat("${strFloat}"): ${parseFloat(strFloat)}`);
-console.log(`"Diferença: parseInt retorna um número inteiro, ignorando o que vem depois do ponto decimal. parseFloat retorna um número com casas decimais."`);s
+pi=4;
+console.log(pi);
+//apareceu um textao dnv de erro, pq como é uma constante ela n pode ser alterada dps da primeira vez colocada
