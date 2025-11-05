0e9019fe4ba292ad47db86a5f1c6cd404a420655
anabsena7-cpu <ana.b.sena7@aluno.senai.br>
2025-11-05 15:57:31 -0300

    Update ex6.js

--- m6/ex6.js
+++ m6/ex6.js
@@ -1,1 +1,11 @@
-
+function safeParse(jsonString) {
+  try {
+    return JSON.parse(jsonString);
+  } catch (erro) {
+    if (erro instanceof SyntaxError) {
+      return null;
+    } else {
+      throw erro; // relança erro inesperado
+    }
+  }
+}
