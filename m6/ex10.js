e814af27590c5c0fac6ccd25e7f2f6e386ea84be
anabsena7-cpu <ana.b.sena7@aluno.senai.br>
2025-11-05 15:58:33 -0300

    Update ex10.js

--- m6/ex10.js
+++ m6/ex10.js
@@ -1,1 +1,7 @@
+// Passos:
+// 1️⃣ Abra o navegador (Chrome, Firefox ou Edge).
+// 2️⃣ Pressione F12 ou Ctrl+Shift+I.
+// 3️⃣ Vá até a aba “Sources” (ou “Depurador”).
+// 4️⃣ Encontre o arquivo JS a ser depurado.
+// 5️⃣ Clique no número da linha para marcar um breakpoint.
 
