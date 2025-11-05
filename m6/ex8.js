6107aac84dc4fda0f750cce193500ff114a60556
anabsena7-cpu <ana.b.sena7@aluno.senai.br>
2025-11-05 15:57:55 -0300

    Update ex8.js

--- m6/ex8.js
+++ m6/ex8.js
@@ -1,1 +1,21 @@
+class InvalidAgeError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "InvalidAgeError";
+  }
+}
 
+function checkAge(age) {
+  if (age < 0 || age > 120) {
+    throw new InvalidAgeError("Idade fora do intervalo");
+  }
+  return "Idade válida";
+}
+
+// Testes
+try {
+  console.log(checkAge(30));  // ✅ Idade válida
+  console.log(checkAge(-5));  // 🚫 Lança erro
+} catch (err) {
+  console.error(err.name + ":", err.message);
+}
