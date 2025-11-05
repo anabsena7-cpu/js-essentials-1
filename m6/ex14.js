d7181ad241580343894e149b6b907728b7104a9d
anabsena7-cpu <ana.b.sena7@aluno.senai.br>
2025-11-05 15:59:29 -0300

    Update ex14.js

--- m6/ex14.js
+++ m6/ex14.js
@@ -1,1 +1,6 @@
-
+// Para retomar execução após breakpoint:
+// ▶ Pressione F8 ou clique no botão “Continue” no DevTools.
+//
+// Para remover todos os breakpoints:
+// 🧹 Vá ao painel “Breakpoints” → clique com o botão direito → “Remove all breakpoints”
+// ou clique novamente nos números das linhas.
