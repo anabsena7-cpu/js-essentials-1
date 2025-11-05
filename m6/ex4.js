0dc35d835c29b76871ae4d5a33d9a1f1b1d636e7
anabsena7-cpu <ana.b.sena7@aluno.senai.br>
2025-11-05 15:55:13 -0300

    Update ex4.js

--- m6/ex4.js
+++ m6/ex4.js
@@ -1,1 +1,9 @@
+// ReferenceError: acessar variável inexistente
+// console.log(x); // ReferenceError
+
+// TypeError: tentar usar método ou função em tipo incorreto
+// let n = null; n.toString(); // TypeError
+
+// SyntaxError: erro na escrita do código (ex: esquecer uma chave)
+// if (condicao { // SyntaxError
 
