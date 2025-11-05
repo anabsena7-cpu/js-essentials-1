7083124bd5dad077a67155409ff8f7073ccd8a9e
anabsena7-cpu <ana.b.sena7@aluno.senai.br>
2025-11-05 15:53:42 -0300

    Update ex1.js

--- m6/ex1.js
+++ m6/ex1.js
@@ -1,1 +1,4 @@
+// 1.1 Erro: situação em que algo não ocorre como esperado no código.
+// 1.2 Exceção: tipo específico de erro que interrompe o fluxo normal do programa.
+// 1.3 Diferença: em linguagem natural, "erro" é qualquer engano; em JS, "exceção" é um erro técnico que o interpretador lança (throw).
 
