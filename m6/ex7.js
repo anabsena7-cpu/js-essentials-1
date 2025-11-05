d24ceddd93f101fa0054b570c64efa07444b78bc
anabsena7-cpu <ana.b.sena7@aluno.senai.br>
2025-11-05 15:57:43 -0300

    Update ex7.js

--- m6/ex7.js
+++ m6/ex7.js
@@ -1,1 +1,16 @@
+function safeParse(jsonString) {
+  try {
+    return JSON.parse(jsonString);
+  } catch (erro) {
+    if (erro instanceof SyntaxError) {
+      return null;
+    } else {
+      throw erro;
+    }
+  } finally {
+    console.log("Parse attempt finished");
+  }
+}
 
+console.log(safeParse('{"nome": "Ana"}'));
+console.log(safeParse("texto inválido"));
